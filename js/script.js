35f8866df106d583b6a55ec435e72d4331d7e804
pielichl <lukaszpielichowski@icloud.com>
2022-04-28 22:44:27 +0200

    Add class active to the correct arcticle

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ function titleClickHandler(){
     const targetArticle = document.querySelector(articleSelector);
     console.log(targetArticle);
 
-  /* [IN PROGRESS] add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
     targetArticle.classList.add('active');
     console.log('Article is visible');
 
