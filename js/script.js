9814083872250a9dfd0c8eb044b4cbc658ee6043
pielichl <lukaszpielichowski@icloud.com>
2022-05-01 12:02:27 +0200

    Add function generateTitleLinks & remove content

--- js/script.js
+++ js/script.js
@@ -45,3 +45,29 @@ function titleClickHandler(){
     link.addEventListener('click', titleClickHandler);
     }
     
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+  
+function generateTitleLinks(customSelector){
+  console.log('Title Lists are just generated')
+  
+  /* [DONE] remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector)
+  titleList.innerHTML = '';
+
+  /* for each article */
+  
+    /* get the article id */
+  
+    /* find the title element */
+  
+    /* get the title from the title element */
+  
+    /* create HTML of the link */
+  
+    /* insert link into titleList */
+  
+  }
+  
+  generateTitleLinks();
