6584426603c2e026939fb2a6b88c744b5d1d4d4f
pielichl <lukaszpielichowski@icloud.com>
2022-05-19 19:35:34 +0200

    Add tag list and categories

--- js/script.js
+++ js/script.js
@@ -75,13 +75,24 @@ function generateTags(){
       let LinkHTMLtag = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
       /* add generated code to html variable */
       html = html + LinkHTMLtag;
+      /* [NEW] check if this link is NOT already in allTags */
+      if(allTags.indexOf(linkHTML) == -1){
+        /* [NEW] add generated code to allTags array */
+        allTags.push(linkHTML);
+      }
     /* END LOOP: for each tag */
     }
     /* insert HTML of all the links into the tags wrapper */
     tagsWrapper.innerHTML = html;
   /* END LOOP: for every article: */
   }
+  /* [NEW] find list of tags in right column */
+  const tagList = document.querySelector(optTagsListSelector);
+
+  /* [NEW] add html from allTags to tagList */
+  tagList.innerHTML = allTags.join(' ');
 }
+
 function tagClickHandler(event){
   /* prevent default action for this event */
   event.preventDefault('');
@@ -185,6 +196,8 @@ function addClickListenersToAuthors(){
     authorAll.addEventListener('click', AuthorClickHandler);
   /* END LOOP: for each link */
   }
+}
+
 }
 
 generateTitleLinks();
